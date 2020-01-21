74ea0b1b6ff0712c2fd5e6d6599c2507a1d0c914
Anders Evenrud <andersevenrud@gmail.com>
2020-01-21 15:40:22 +0100

    Updated exports

--- index.js
+++ index.js
@@ -29,6 +29,10 @@
  */
 
 const Core = require('./src/core.js');
+const Auth = require('./src/auth.js');
+const Filesystem = require('./src/filesystem.js');
+const Settings = require('./src/settings.js');
+const Packages = require('./src/packages.js');
 const CoreServiceProvider = require('./src/providers/core');
 const PackageServiceProvider = require('./src/providers/packages');
 const VFSServiceProvider = require('./src/providers/vfs');
@@ -37,6 +41,10 @@ const SettingsServiceProvider = require('./src/providers/settings');
 
 module.exports = {
   Core,
+  Auth,
+  Filesystem,
+  Settings,
+  Packages,
   CoreServiceProvider,
   PackageServiceProvider,
   VFSServiceProvider,
